e52d9f19f13b364e59969e0977626c43e8566c99
adrielripoll <adrielripoll@hotmail.com>
2023-01-17 14:12:30 -0300

    feat: add getters

--- src/entities/server-queue.entity.ts
+++ src/entities/server-queue.entity.ts
@@ -7,7 +7,39 @@ export class ServerQueue {
         this.props = {}
     }
 
+    public get value(): Partial<IServerQueueProps>{
+        return this.props
+    }
+
+    public get musicQueue(){
+        return this.props.musicQueue
+    }
+
+    public get guild(){
+        return this.props.guild
+    }
+
+    public get voiceChannel(){
+        return this.props.voiceChannel
+    }
+
+    public get textChannel(){
+        return this.props.textChannel
+    }
+
+    public get volume(){
+        return this.props.volume
+    }
+
+    public get playing(){
+        return this.props.volume
+    }
+    
     public config(props: IServerQueueProps){
         this.props = props
+    }
+    
+    public delete(){
+        this.props = {}
     }
 }
